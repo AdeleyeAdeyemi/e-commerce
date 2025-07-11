0b3dede56ce0583ae5c496a9573805b896ed9441
onisowo <adeley.adeyemi@onisowo.com>
2025-07-11 19:36:21 +0100

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "flask_app" {
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (London Region)
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "flask-ecommerce-app"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '[web]\n${self.public_ip}' > ../ansible/inventory.ini"
+  }
+}
