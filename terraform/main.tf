8d3ecd124aabae3753550939e2cfb38656c05189
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-07-11 21:22:05 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,10 +20,21 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
+# Generate SSH keypair inside Terraform
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "terraform-generated-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
 resource "aws_instance" "flask_app" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
-  key_name                    = var.key_name
+  key_name                    = aws_key_pair.generated_key.key_name
   associate_public_ip_address = true
 
   tags = {
@@ -33,4 +44,9 @@ resource "aws_instance" "flask_app" {
   provisioner "local-exec" {
     command = "echo '[web]\n${self.public_ip}' > ../ansible/inventory.ini"
   }
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.my_key.private_key_pem
+  sensitive = true
 }
