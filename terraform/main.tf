7ce6a3d7f52315ee0aa1232a3be440a5bea8e175
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-08-22 10:17:23 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,31 +4,37 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+# Generate SSH key
 resource "tls_private_key" "terraform_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Create AWS key pair
 resource "aws_key_pair" "terraform_key" {
-  key_name   = "terraform-generated-key"
+  key_name   = var.key_name
   public_key = tls_private_key.terraform_key.public_key_openssh
 }
 
+# Security Group
 resource "aws_security_group" "flask_sg" {
   name        = "flask-app-sg"
   description = "Allow SSH and HTTP"
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 8777
     to_port     = 8777
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -37,6 +43,18 @@ resource "aws_security_group" "flask_sg" {
   }
 }
 
+# Find latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# EC2 instance
 resource "aws_instance" "flask_app" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
@@ -46,6 +64,19 @@ resource "aws_instance" "flask_app" {
   tags = {
     Name = "flask-app"
   }
+}
+
+# Output public IP
+output "public_ip" {
+  description = "Public IP of the Flask app"
+  value       = aws_instance.flask_app.public_ip
+}
+
+# Output private key for Jenkins/Ansible
+output "private_key_pem" {
+  description = "Private key for SSH access"
+  value       = tls_private_key.terraform_key.private_key_pem
+  sensitive   = true
 }
 
 
