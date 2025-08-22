63f7b4cb360918aba7c22eea241415fe183ede54
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-08-22 10:39:11 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,12 @@
+# Output public IP of the Flask app
 output "public_ip" {
   description = "Public IP of the Flask app"
   value       = aws_instance.flask_app.public_ip
+}
+
+# Output private key for Jenkins/Ansible (do NOT store locally)
+output "private_key_pem" {
+  description = "Private key for SSH access"
+  value       = tls_private_key.terraform_key.private_key_pem
+  sensitive   = true
 }
