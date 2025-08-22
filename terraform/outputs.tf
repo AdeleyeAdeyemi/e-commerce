45e575d3af83e6ecd37474d466f5df757aa179cc
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-08-22 18:13:35 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,16 +6,9 @@ output "public_ip" {
   value       = aws_instance.flask_app.public_ip
 }
 
-output "terraform_key_pem" {
-  description = "Private key for SSH access"
-  value       = tls_private_key.terraform_key.private_key_pem
-  sensitive   = true
-}
-
 output "security_group_id" {
   description = "ID of the Flask app security group"
   value       = aws_security_group.flask_sg.id
 }
-
 
 
