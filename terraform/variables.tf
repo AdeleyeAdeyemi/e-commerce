02e11d2a6d1560af0725afae557ff5ed449f788b
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-08-24 10:09:05 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,8 @@
-##############################
-# VARIABLES
-##############################
+
 variable "region" {
   type        = string
   description = "AWS region"
   default     = "eu-west-2"
 }
 
-variable "aws_access_key" {
-  type        = string
-  description = "AWS access key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS secret key"
-  sensitive   = true
-}
-
-variable "key_name" {
-  type        = string
-  description = "EC2 Key Pair name"
-}
 
