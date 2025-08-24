f20bf4b50df19e47e57f6dca06e2f1bef9a7dfbb
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-08-24 09:35:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,29 @@
-##############################
-# PROVIDER
-##############################
 provider "aws" {
-  region     = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-west-2"
 }
 
-##############################
-# SECURITY GROUP
-##############################
-data "aws_vpc" "default" {
-  default = true
+# Generate new key pair each run
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-resource "aws_security_group" "flask_sg" {
-  name        = "flask-app-sg"
-  description = "Allow SSH, Flask app, and ELK ports"
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_key_pair" "generated" {
+  key_name   = "jenkins-key-${formatdate("YYYYMMDDHHmmss", timestamp())}"
+  public_key = tls_private_key.generated.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.generated.private_key_pem
+  filename        = "${path.module}/jenkins-key.pem"
+  file_permission = "0400"
+}
+
+# Security group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow SSH and HTTP"
+
 
   ingress {
     from_port   = 22
@@ -82,23 +88,16 @@ data "aws_ami" "amazon_linux" {
 ##############################
 # EC2 INSTANCE
 ##############################
-resource "aws_instance" "flask_app" {
+resource "aws_instance" "ec2" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
-  key_name                    = var.key_name   # Use the key passed from Jenkins
-  vpc_security_group_ids      = [aws_security_group.flask_sg.id]
+  key_name                    = aws_key_pair.generated.key_name
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "flask-app"
+    Name = "jenkins_ec2"
   }
-}
-
-##############################
-# OUTPUTS
-##############################
-output "instance_public_ip" {
-  value = aws_instance.flask_app.public_ip
 }
 
 
