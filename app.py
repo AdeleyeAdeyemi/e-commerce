6ccb481914bd9bb9cc862414210c68e4651d6e6e
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-08-28 13:43:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,9 +73,10 @@ def clear_cart():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8777)
+    app.run(host='0.0.0.0', port=8777, debug=False)
 
 
   
+
 
 
