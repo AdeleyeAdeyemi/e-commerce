69d7c03ad577a99f7d33bc44794587392f6de876
AdeleyeAdeyemi <josekadel355@gmail.com>
2026-09-05 18:03:25 +0100

    Fix Terraform root provider configuration

---
+++ Terraform_module/terraform_project/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  common_tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    Owner       = var.owner
+    Application = var.application
+    ManagedBy   = "Terraform"
+  }
+}
